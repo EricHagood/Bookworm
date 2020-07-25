e37a6bfc1a877bd2ec68cc6b5cf32525648e59ba
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-07-24 20:36:42 -0500

    added style for my collection

--- bookworm-client/src/components/HomeView.jsx
+++ bookworm-client/src/components/HomeView.jsx
@@ -4,24 +4,23 @@ export default class HomeView extends Component {
   
     render() {
         return (this.props.books.map( (book, index) => {
-            return (<div key={index}>
-              <div onClick={ () => { this.props.clickOnBook(book) } }>
+            return(<div className='column-container' key={index}>
+              <div className='book-container' onClick={ () => { this.props.clickOnBook(book) } }>
                 <p className='book-text'>Title: {book.title}</p>
-                <p className='book-tex'>Authors: { book.authors.toString() }</p>        
+                <p className='book-text'>Authors: { book.authors.toString() }</p>        
                 <img src = {book.thumbnail} className="thumbnail" alt="books"></img>
-              </div>
-              {
-                  !book.myCollection ?
-                     <button onClick = {()=> {  book.myCollection = !book.myCollection; this.props.updateBook(book, index)}}>Add to Collection </button> : ''
-              }
+              
+             <div className='button-container'>
               {
                 book.isFavorite ?
-                  <button onClick={()=> {  book.isFavorite = !book.isFavorite; this.props.updateBook(book, index)}}>Unfavorite</button> : <button onClick={()=> {book.isFavorite = !book.isFavorite; this.props.updateBook(book, index)}}>Favorite</button>
+                  <button className='collection-button' onClick={()=> {  book.isFavorite = !book.isFavorite; this.props.updateBook(book, index)}}>Unfavorite</button> : <button className='collection-button' onClick={()=> {book.isFavorite = !book.isFavorite; this.props.updateBook(book, index)}}>Favorite</button>
               }
-              <button onClick={()=> { this.props.deleteBook( book, index ) }}>Delete</button>
+              <button className='collection-button' onClick={()=> { this.props.deleteBook( book, index ) }}>Delete</button>
+              </div>
+              </div>
               </div>
               )
-            
+              
           }))
     }
 }
