f343378b5619921ef904dfe1fe9381a8373165a1
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-07-27 11:13:51 -0500

    favorite page styling added

--- bookworm-client/src/components/FavoriteView.jsx
+++ bookworm-client/src/components/FavoriteView.jsx
@@ -6,9 +6,9 @@ export default class FavoriteView extends Component {
         return (this.props.books.map( (book, index) => {
             return (<div className='column-container' key={index}>
               <div className="book-container" onClick={ () => { this.props.clickOnBook(book) } }>
-                <p>Title: {book.title}</p>
-                <p>Authors: { book.authors.toString() }</p>        
-                <img src = {book.thumbnail} alt="books"></img>
+                <p className="book-text">Title: {book.title}</p>
+                <p className="book-text">Authors: { book.authors.toString() }</p>        
+                <img className="thumbnail" src = {book.thumbnail} alt="books"></img>
               </div>
               </div>
               )
